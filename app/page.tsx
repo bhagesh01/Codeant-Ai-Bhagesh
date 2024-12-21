de54d29ad1a0d42547c5580663c6bc6e7e2f6dd3
bhagesh01 <bansodebhageshbb08@gmail.com>
2024-12-21 22:06:34 +0530

    commit for deploy

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ const Home = () => {
             <h1>
               Experience the
               <span className="text-transparent px-2 bg-gradient-to-r from-[#D247BF] to-primary bg-clip-text">
-                Bobby's
+                Bobby&apos;s
               </span>
               landing page
             </h1>
