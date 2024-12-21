0d53f5ec5bf8607ce17d95315471b4c8033e943e
bhagesh01 <bansodebhageshbb08@gmail.com>
2024-12-21 22:08:50 +0530

    commit for deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Poppins } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
 const poppins = Poppins({
@@ -8,17 +8,6 @@ const poppins = Poppins({
   variable: '--font-poppins',
 });
 
-
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
